0a2de16e9429f834dc6a7b80cc483e2eb6f6f029
Cigan <n.ivanov@hookprod.com>
2020-05-28 18:31:56 +0700

    prepare for use api

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Login = () => {
-    return <p>Login page</p>;
+    const [loginForm, setLoginForm] = useState({ name: '', password: '' });
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log('Cigan-log: submitForm -> loginForm', loginForm);
+        setLoginForm({ name: '', password: '' });
+    };
+    return (
+        <div>
+            <p>Login page</p>
+            <form onSubmit={(e) => submitForm(e)}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={loginForm.name}
+                    onChange={(e) =>
+                        setLoginForm({ ...loginForm, name: e.target.value })
+                    }
+                />
+                <input
+                    type="text"
+                    placeholder="password"
+                    value={loginForm.password}
+                    onChange={(e) =>
+                        setLoginForm({ ...loginForm, password: e.target.value })
+                    }
+                />
+                <button type="submit">Sign In</button>
+            </form>
+        </div>
+    );
 };
--- frontend/src/register.js
+++ frontend/src/register.js
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Register = () => {
-    return <p>Страница регистрации</p>;
+    const [registerForm, setRegisterForm] = useState({
+        name: '',
+        password: '',
+    });
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log('Cigan-log: submitForm -> registerForm', registerForm);
+        setRegisterForm({ name: '', password: '' });
+    };
+    return (
+        <div>
+            <p>Страница регистрации</p>
+            <form onSubmit={(e) => submitForm(e)}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={registerForm.name}
+                    onChange={(e) =>
+                        setRegisterForm({
+                            ...registerForm,
+                            name: e.target.value,
+                        })
+                    }
+                />
+                <input
+                    type="text"
+                    placeholder="password"
+                    value={registerForm.password}
+                    onChange={(e) =>
+                        setRegisterForm({
+                            ...registerForm,
+                            password: e.target.value,
+                        })
+                    }
+                />
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
 };
